aa044abed2de14c6cda5042b26ca11687eb0a077
LilyMaster <lilymaster@gmail.com>
2018-09-09 20:47:33 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ChallongeAPIcaller',
+    version='0.2',
+    packages=['src.challongeviewer'],
+    url='',
+    license='',
+    author='Martynas Saulius',
+    author_email='martynas575@gmail.com',
+    description='Challonge API caller and parser'
+)
